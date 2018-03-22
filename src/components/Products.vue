a4db25ac3e58fef4e631df725e4d28f13c81dbdc
Robley Gori <robleyadrian@gmail.com>
2018-03-22 12:21:14 +0300

    Initial work

---
+++ src/components/Products.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 class="title"> Products </h1>
+      <p> {{ length }} products </p>
+      <table class="table is-striped">
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id" track-by="id">
+            <td>{{ product.name }}</td>
+            <td>{{ product.description }}</td>
+            <td>${{ product.price }}</td>
+            <td> <button @click='addToCart(product)' class="button is-info">Add to cart</button> </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'app',
+  computed: mapGetters({
+    products: 'allProducts',
+    length: 'getNumberOfProducts'
+  }),
+  methods: mapActions([
+    'addToCart'
+  ])
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
